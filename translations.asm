f10d42994908bf187b7a9a07a5e529dd37385126
Reed Thorngag <reed.thorngag@gmail.com>
2022-08-22 22:53:55 +1200

    .

--- translations.asm
+++ translations.asm
@@ -1,8 +1,11 @@
 
-; b0 61		; mov al,61
-; b4 0e		; mov ah,0x0e
-; cd 10		; int 0x10
-; eb fe		; jmp $
+; b0 61		    ; mov al,61
+; b4 0e		    ; mov ah,0x0e
+; ba 70 00      ; mov dx,hex_characters (a label) (70 is the position of the label from the start 0f the file)
+; 8b 16 70 00   ; mov dx,[hex_characters]
+; cd 10		    ; int 0x10
+; eb fe		    ; jmp $
+; c3            ; ret
 
 
 
