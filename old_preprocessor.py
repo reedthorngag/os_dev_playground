f8bce525dc323d1a94600ac3b53927f3c9a457d4
Brody Cooper <reed.thorngag@gmail.com>
2023-05-07 11:51:52 +1200

    added a version of the old preprocessor

---
+++ old_preprocessor.py
@@ -0,0 +1,23 @@
+
+def process(input_dir,input,output):
+
+    input = open(input_dir+'/'+input,'r')
+    for line in input:
+        if line[0:9]=='#include ':
+            output.write('\n')
+            file_pieces = line.split('"')[1].split('/')
+            file = file_pieces[-1]
+            dir = input_dir + ('/' + '/'.join(file_pieces[:-1]) if len(file_pieces[:-1])!=0 else '')
+            try:
+                process(dir,file,output)
+                output.write('\n')
+            except:
+                print("Couldn't open '"+dir+'/'+file+"' ("+str(file_pieces)+")!")
+        
+        elif line!='\n':
+            output.write(line)
+            
+    input.close()
+
+output = open('cdiso/output.asm','w')
+process('src','os.asm',output)
