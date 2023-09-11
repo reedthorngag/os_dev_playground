72a68e3029826b8b58ac32c11b911daa932cef10
Brody Cooper <reed.thorngag@gmail.com>
2023-09-12 04:44:03 +1200

    .

--- src/long_mode_os/bootloader/get_mem_map.asm
+++ src/long_mode_os/bootloader/get_mem_map.asm
@@ -1,58 +1,55 @@
 
 get_mem_map:
-
-    xor ax,ax
-    mov es,ax
-    mov di, mem_map_buffer
-    xor ebx,ebx
-    xor bp,bp
-
-.loop:
-    mov edx, 0x0534D4150
-    mov eax, 0xE820
-    mov ecx,24
-    mov dword [es:di+20],1
-
-    int 0x15
-
-    jc .end
-    cmp eax,'PAMS'
-    jne .error
-    jcxz .loop
-    cmp cl,24
-    jb .loop
-
-    mov ecx, [es:di+8]
-    or ecx, [es:di+12]
-    je .loop
-
-    inc bp
-    add di,24
-    cmp di, mem_map_buffer_end
-    jge .out_of_space_err
-
-.end:
-    mov word [mem_map_size],bp
-    mov bx,di
-    call print_hex
-    mov bx,mem_map_buffer
-    call print_hex
-    call pause
-    ret
-
-.error:
+    mov di, mem_map_buffer       ; Set di to 0x8004. Otherwise this code will get stuck in `int 0x15` after some entries are fetched 
+	xor ebx, ebx		; ebx must be 0 to start
+	xor bp, bp		; keep an entry count in bp
+	mov edx, 0x0534D4150	; Place "SMAP" into edx
+	mov eax, 0xe820
+	mov dword [es:di + 20], 1	; force a valid ACPI 3.X entry
+	mov ecx, 24		; ask for 24 bytes
+	int 0x15
+	jc short .failed	; carry set on first call means "unsupported function"
+	mov edx, 0x0534D4150	; Some BIOSes apparently trash this register?
+	cmp eax, edx		; on success, eax must have been reset to "SMAP"
+	jne short .failed
+	test ebx, ebx		; ebx = 0 implies list is only 1 entry long (worthless)
+	je short .failed
+	jmp short .jmpin
+.e820lp:
+	mov eax, 0xe820		; eax, ecx get trashed on every int 0x15 call
+	mov dword [es:di + 20], 1	; force a valid ACPI 3.X entry
+	mov ecx, 24		; ask for 24 bytes again
+	int 0x15
+	jc short .e820f		; carry set means "end of list already reached"
+	mov edx, 0x0534D4150	; repair potentially trashed register
+.jmpin:
+	jcxz .skipent		; skip any 0 length entries
+	cmp cl, 20		; got a 24 byte ACPI 3.X response?
+	jbe short .notext
+	test byte [es:di + 20], 1	; if so: is the "ignore this data" bit clear?
+	je short .skipent
+.notext:
+	mov ecx, [es:di + 8]	; get lower uint32_t of memory region length
+	or ecx, [es:di + 12]	; "or" it with upper uint32_t to test for zero
+	jz .skipent		; if length uint64_t is 0, skip entry
+	inc bp			; got a good entry: ++count, move to next storage spot
+	add di, 24
+    cmp di,mem_map_buffer_end
+    jbe .ran_out_of_space
+.skipent:
+	test ebx, ebx		; if ebx resets to 0, list is complete
+	jne short .e820lp
+.e820f:
+	mov word [mem_map_size], bp	; store the entry count
+	ret
+.failed:
     mov si, .gmm_err_str
     call print_str
-    mov bx,ax
-    shr bx,8
-    call print_hex
     call hang
-
-.out_of_space_err:
-    mov si, .out_of_space_err_str
+.ran_out_of_space:
+    mov si, .out_of_space_str
     call print_str
     call hang
 
-
-.gmm_err_str: db 'get mem map err, code: ',0
+.gmm_err_str: db 'function unsupported! ',0
 .out_of_space_err_str: db 'ran out of space to read mem map into!',0
